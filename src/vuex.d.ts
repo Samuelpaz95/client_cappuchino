158911519149b822123a574a3d89c7bc024743c7
Alan Benavides <bruno@coastt.org>
2021-07-26 18:34:48 -0400

    change: types in store as any

--- src/vuex.d.ts
+++ src/vuex.d.ts
@@ -2,13 +2,8 @@ import { ComponentCustomProperties } from "vue";
 import { Store } from "vuex";
 
 declare module "@vue/runtime-core" {
-	// declare your own store states
-	interface State {
-		count: number;
-	}
-
 	// provide typings for `this.$store`
 	interface ComponentCustomProperties {
-		$store: Store<State>;
+		$store: Store<any>;
 	}
 }
