e44396f07e9fedd5996c4cb1a565af452293a36e
Alan Benavides <bruno@coastt.org>
2021-07-26 16:32:55 -0400

    add: view teacher tracker

---
+++ src/views/teacher-tracker/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<router-view />
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "TracherDocent",
+	});
+</script>
+
+<style scoped lang="scss"></style>
