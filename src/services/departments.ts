a1a916347bcb8c3989d13dd68b014a8afaaf2dfc
Alan Benavides <bruno@coastt.org>
2021-07-26 16:24:24 -0400

    add: departments service

---
+++ src/services/departments.ts
@@ -0,0 +1,38 @@
+import { http } from "./httpRequest";
+import { Icarrer, Idepartment, IdepartementCarrer } from "../interfaces";
+
+class DepartmentsService {
+	private readonly breakPoint = "departments";
+
+	async getAllDepartments(): Promise<Idepartment[] | null> {
+		try {
+			const { data }: { data: Idepartment[] } = await http.get(`/${this.breakPoint}`);
+			return data;
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+
+	async getDepartmentCarrers(department: string): Promise<IdepartementCarrer[] | null> {
+		try {
+			const { data }: { data: IdepartementCarrer[] } = await http.get(`/${this.breakPoint}/${department}`);
+			return data;
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+
+	async getDepartmentCarrer(department: string, carrer: string): Promise<Icarrer | null> {
+		try {
+			const { data }: { data: Icarrer } = await http.get(`/${this.breakPoint}/${department}/${carrer}`);
+			return data;
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+}
+
+export default new DepartmentsService();
