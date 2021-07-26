537eb2b76b0fc315f3bbe9e5baa303efa2152a32
Alan Benavides <bruno@coastt.org>
2021-07-26 18:01:47 -0400

    add: create component header

---
+++ src/components/frontend/BaseHeader.vue
@@ -0,0 +1,41 @@
+<template>
+	<header class="header">
+		<router-link class="header__linkmain" to="/">
+			<h2 class="header__title">TITLE</h2>
+		</router-link>
+	</header>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		setup() {
+			return {};
+		},
+	});
+</script>
+
+<style scoped lang="scss">
+	@import "../../scss/abstracts/_variables.scss";
+
+	.header {
+		width: 100%;
+		height: 100%;
+		background-color: $primary_color;
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		box-shadow: $box-shadow;
+
+		&__linkmain {
+			text-decoration: none;
+			padding-left: 3rem;
+		}
+
+		&__title {
+			font-size: 2rem;
+			color: $font_color;
+		}
+	}
+</style>
