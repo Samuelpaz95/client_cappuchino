f48f4fdc8fc9652273bb48cc70800ed1d7760ea1
Alan Benavides <bruno@coastt.org>
2021-07-26 16:48:00 -0400

    add: principal layout

---
+++ src/layouts/Frontend.vue
@@ -0,0 +1,14 @@
+<template>
+	Frontend
+	<router-view />
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "Frontend",
+	});
+</script>
+
+<style scoped lang="scss"></style>
