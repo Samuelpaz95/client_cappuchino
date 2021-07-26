2a1352f5f76453ada3ed7ce2b4183e7793cac300
Alan Benavides <bruno@coastt.org>
2021-07-26 17:18:26 -0400

    add: page 404

---
+++ src/views/404/index.vue
@@ -0,0 +1,11 @@
+<template>Pagina no encontrada :p</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "404",
+	});
+</script>
+
+<style scoped lang="scss"></style>
