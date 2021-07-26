b72a02ff7d07c5ae79b7d4a769d4ba2965573fe2
Alan Benavides <bruno@coastt.org>
2021-07-26 18:33:21 -0400

    refactor: create component footer links

--- src/components/frontend/BaseFooter.vue
+++ src/components/frontend/BaseFooter.vue
@@ -1,15 +1,7 @@
 <template>
 	<footer class="footer">
 		<div class="footer__container">
-			<span>
-				<ul class="footer__links">
-					<li class="footer__link" v-for="(value, index) in links" :key="index">
-						<a :href="value.route" target="_blank" :hreflang="value.alt" referrerpolicy="no-referrer">
-							<BaseSystemIcons :icon="value.alt" />
-						</a>
-					</li>
-				</ul>
-			</span>
+			<BaseFooterListLinks />
 			<img class="footer__image" src="../../assets/scesi_white.png" alt="scesi" />
 		</div>
 	</footer>
@@ -18,40 +10,17 @@
 <script lang="ts">
 	import { defineComponent } from "vue";
 	import BaseSystemIcons from "../BaseSystemIcons.vue";
+	import BaseFooterListLinks from "./BaseFooterListLinks.vue";
 
 	export default defineComponent({
 		components: {
 			BaseSystemIcons,
-		},
-		setup() {
-			const links: { alt: string; route: string }[] = [
-				{
-					alt: "facebook",
-					route: "https://www.facebook.com/scesi",
-				},
-				{
-					alt: "instagram",
-					route: "https://www.instagram.com/scesiumss/?hl=es",
-				},
-				{
-					alt: "twitter",
-					route: "https://twitter.com/ScesiU",
-				},
-				{
-					alt: "github",
-					route: "https://github.com/scesi",
-				},
-				{
-					alt: "youtube",
-					route: "https://www.youtube.com/channel/UCfhSc_xLVxFubp9exyacy0Q",
-				},
-			];
-			return { links };
+			BaseFooterListLinks,
 		},
 	});
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 	@import "../../scss/abstracts/_variables.scss";
 	@import "../../scss/abstracts/_mixins.scss";
 
@@ -70,56 +39,17 @@
 			width: 100%;
 			height: 100%;
 			padding: 0 1rem;
-		}
 
-		&__links {
-			display: flex;
-			padding: 0;
-		}
-
-		&__link {
-			position: relative;
-			bottom: -6px;
-			padding: 0 5px;
-			list-style: none;
-
-			:link {
-				color: $font_color;
-			}
-
-			:visited {
-				color: $font_color;
-			}
-
-			svg {
-				height: 24px;
-			}
-		}
-
-		&_image {
-			width: 71px;
-			padding-right: calc(26px - 1rem);
-		}
-
-		@include large {
-			&__container {
+			@include large {
 				padding: 0 2rem;
 			}
+		}
 
-			&__link {
-				padding: 0 10px;
+		&__image {
+			width: 80px;
+			padding-right: calc(26px - 1rem);
 
-				svg {
-					height: 29;
-				}
-			}
-
-			&__links::before {
-				content: "Siguenos en:";
-				font-size: 18px;
-			}
-
-			&__image {
+			@include large {
 				width: 80px;
 			}
 		}
---
+++ src/components/frontend/BaseFooterListLinks.vue
@@ -0,0 +1,82 @@
+<template>
+	<ul class="footer-links">
+		<li class="footer-links__link" v-for="(value, index) in links" :key="index">
+			<a :href="value.route" target="_blank" :hreflang="value.alt" referrerpolicy="no-referrer">
+				<BaseSystemIcons :icon="value.alt" />
+			</a>
+		</li>
+	</ul>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+	import BaseSystemIcons from "../BaseSystemIcons.vue";
+
+	export default defineComponent({
+		components: {
+			BaseSystemIcons,
+		},
+		setup() {
+			const links: { alt: string; route: string }[] = [
+				{
+					alt: "facebook",
+					route: "https://www.facebook.com/scesi",
+				},
+				{
+					alt: "instagram",
+					route: "https://www.instagram.com/scesiumss/?hl=es",
+				},
+				{
+					alt: "twitter",
+					route: "https://twitter.com/ScesiU",
+				},
+				{
+					alt: "github",
+					route: "https://github.com/scesi",
+				},
+				{
+					alt: "youtube",
+					route: "https://www.youtube.com/channel/UCfhSc_xLVxFubp9exyacy0Q",
+				},
+			];
+			return { links };
+		},
+	});
+</script>
+
+<style scoped lang="scss">
+	@import "../../scss/abstracts/_variables.scss";
+	@import "../../scss/abstracts/_mixins.scss";
+
+	.footer-links {
+		display: flex;
+		padding: 0;
+
+		&__link {
+			position: relative;
+			bottom: -6px;
+			padding: 0 5px;
+			list-style: none;
+
+			:link {
+				color: $font_color;
+			}
+
+			:visited {
+				color: $font_color;
+			}
+
+			@include large {
+				padding: 0 10px;
+			}
+		}
+
+		@include large {
+			&::before {
+				content: "Siguenos en:";
+				font-size: 18px;
+				padding-right: 1rem;
+			}
+		}
+	}
+</style>
