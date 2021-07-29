752448c0a544c581ead431118f3c91be217a3f3b
Alan Benavides <bruno@coastt.org>
2021-07-29 12:05:24 -0400

    add: insert component inputSearch in docent-tracker

--- src/views/teacher-tracker/index.vue
+++ src/views/teacher-tracker/index.vue
@@ -1,13 +1,43 @@
 <template>
-	<div></div>
+	<main class="tracker">
+		<section class="tracker__container">
+			<InputSearch class="tracker__search" :listOfItems="['1', '2']" />
+		</section>
+	</main>
 </template>
 
 <script lang="ts">
 	import { defineComponent } from "vue";
+	import InputSearch from "../../components/InputSearch.vue";
 
 	export default defineComponent({
 		name: "TracherDocent",
+		components: {
+			InputSearch,
+		},
 	});
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	@import "../../scss/abstracts/mixins";
+
+	.tracker {
+		margin: 1rem;
+
+		&__container {
+			display: grid;
+
+			@include large {
+				grid-template-columns: 0.54fr 1fr;
+			}
+		}
+
+		&__search {
+			margin: 15px 30px;
+
+			@include large {
+				margin: 0;
+			}
+		}
+	}
+</style>
