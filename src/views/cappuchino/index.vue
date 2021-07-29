f616f884d6e83530b05e6a733f4c8b56f512c45f
Alan Benavides <bruno@coastt.org>
2021-07-29 12:05:45 -0400

    add: get department info

--- src/views/cappuchino/index.vue
+++ src/views/cappuchino/index.vue
@@ -3,10 +3,19 @@
 </template>
 
 <script lang="ts">
-	import { defineComponent } from "vue";
+	import { defineComponent, onMounted } from "vue";
+	import { useRoute } from "vue-router";
+	import { useStore } from "vuex";
 
 	export default defineComponent({
 		name: "Cappuchino",
+		setup() {
+			const store = useStore();
+			const { params } = useRoute();
+			onMounted(async () => {
+				await store.dispatch("departments/actionGetDepartmentDescription", params.department);
+			});
+		},
 	});
 </script>
 
