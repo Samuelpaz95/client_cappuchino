a1480478c9de1939e3a19c20075d91c14ec5f395
Alan Benavides <bruno@coastt.org>
2021-08-01 11:51:35 -0400

    add: list carrer header

--- src/components/ListCarrers.vue
+++ src/components/ListCarrers.vue
@@ -1,44 +1,70 @@
 <template>
-	<ul class="list-carrers">
-		<ListItemCarrer
-			v-for="(value, index) in departmentCarrers"
-			:key="index"
-			:nameCarrer="value.name"
-			:updateAt="value.semester"
-			@carrer:select="selectCarrer"
-		/>
-	</ul>
+	<div class="list-carrers">
+		<ListCarrersHeader class="list-carrers__header" @menu:open="showCarrers" />
+		<ul class="list-carrers__list" v-show="isShowCarrers">
+			<ListItemCarrer
+				v-for="(value, index) in departmentCarrers"
+				:key="index"
+				:nameCarrer="value.name"
+				:updateAt="value.semester"
+				@carrer:select="selectCarrer"
+			/>
+		</ul>
+	</div>
 </template>
 
 <script lang="ts">
-	import { computed, defineComponent } from "vue";
+	import { computed, defineComponent, ref, Ref } from "vue";
 	import { useStore } from "vuex";
 
 	import ListItemCarrer from "./ListItemCarrer.vue";
+	import ListCarrersHeader from "./ListCarrersHeader.vue";
 
 	export default defineComponent({
 		name: "ListCarrers",
 		components: {
 			ListItemCarrer,
+			ListCarrersHeader,
 		},
 		setup() {
 			const store = useStore();
 			const departmentCarrers = computed(() => store.getters["departments/indexCarrersInfo"]);
+			let isShowCarrers: Ref<boolean> = ref(false);
 
 			const selectCarrer = (nameCarrer: string) => {
 				store.dispatch("departments/actionGetDepartmentCarrer", nameCarrer);
 			};
 
+			const showCarrers = (isOpen: boolean) => {
+				isShowCarrers.value = isOpen;
+			};
+
 			return {
 				departmentCarrers,
+				isShowCarrers,
 				selectCarrer,
+				showCarrers,
 			};
 		},
 	});
 </script>
 
 <style scoped lang="scss">
+	@import "../scss/abstracts/variables.scss";
+
 	.list-carrers {
-		padding: 1rem;
+		background-color: $primary_color;
+
+		&__list {
+			padding: 1rem;
+			margin: 0;
+			width: 100%;
+			background-color: $primary_color;
+			position: absolute;
+			z-index: 10;
+		}
+		&__header {
+			position: relative;
+		}
 	}
 </style>
---
+++ src/components/ListCarrersHeader.vue
@@ -0,0 +1,56 @@
+<template>
+	<header class="list-carrers-header">
+		<button class="list-carrers-header__option" @click="openMenu">
+			<BaseSystemIcons :icon="typeIcon" />
+		</button>
+	</header>
+</template>
+
+<script lang="ts">
+	import { computed, defineComponent, Ref, ref } from "vue";
+
+	import BaseSystemIcons from "./BaseSystemIcons.vue";
+
+	export default defineComponent({
+		name: "ListCarrersHeader",
+		components: {
+			BaseSystemIcons,
+		},
+		emits: ["menu:open"],
+		setup(_, { emit }) {
+			let isMenuOpen: Ref<boolean> = ref(false);
+			const icons: Ref<string[]> = ref(["bars", "close"]);
+
+			const openMenu = () => {
+				isMenuOpen.value = !isMenuOpen.value;
+				emit("menu:open", isMenuOpen.value);
+			};
+
+			const typeIcon = computed(() => icons.value[Number(isMenuOpen.value)]);
+
+			return {
+				isMenuOpen,
+				openMenu,
+				typeIcon,
+			};
+		},
+	});
+</script>
+
+<style scoped lang="scss">
+	@import "../scss/abstracts/variables.scss";
+
+	.list-carrers-header {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		padding: 0.5rem 1rem;
+		background-color: $primary_color;
+
+		&__option {
+			background-color: inherit;
+			border: none;
+			color: inherit;
+		}
+	}
+</style>
