d0b07100bcdd67be1ac184b02feb2bad9b3fd770
Alan Benavides <bruno@coastt.org>
2021-08-08 16:52:11 -0400

    add: complete group item

--- src/components/listCappuchino/listSubjects/listSubjects/GroupItem.vue
+++ src/components/listCappuchino/listSubjects/listSubjects/GroupItem.vue
@@ -1,8 +1,12 @@
 <template>
 	<li class="group-item">
-		<!-- TODO: Si el tamnio del nombre es mucho cortarlo -->
-		<span>{{ nameTeacher }}</span>
-		<span>{{ groupCode }}</span>
+		<input v-model="asd" class="group-item__radio" type="checkbox" :id="groupCode + nameTeacher" />
+		<label class="group-item__label" :for="groupCode + nameTeacher">
+			<span class="group-item__description">
+				<span class="group-item__teacher">{{ transformNameTeacherToCapitalize }}</span>
+				<span class="group-item__group">{{ groupCode }}</span>
+			</span>
+		</label>
 	</li>
 </template>
 
@@ -11,6 +15,11 @@
 
 	export default defineComponent({
 		name: "GroupItem",
+		data() {
+			return {
+				asd: "",
+			};
+		},
 		props: {
 			nameTeacher: {
 				type: String,
@@ -19,13 +28,57 @@
 			},
 			groupCode: String,
 		},
+		computed: {
+			transformNameTeacherToCapitalize(): string {
+				const lower: string = this.nameTeacher.toLowerCase();
+				const arrayLowerName: string[] = lower.split(" ");
+				const capitalizeArrayName: string[] = arrayLowerName.map(
+					(name: string) => name[0].toUpperCase() + name.slice(1)
+				);
+				return capitalizeArrayName.join(" ");
+			},
+		},
 	});
 </script>
 
 <style lang="scss" scoped>
 	.group-item {
 		display: flex;
-		justify-content: space-between;
+		line-height: 24px;
 		padding: 0 1rem;
+
+		&__teacher {
+			overflow: hidden;
+		}
+
+		&__description {
+			width: 100%;
+			display: flex;
+			justify-content: space-between;
+		}
+
+		&__label {
+			display: flex;
+			align-items: center;
+			width: 100%;
+
+			&::before {
+				content: "";
+				display: block;
+				width: 13px;
+				height: 13px;
+				margin-right: 10px;
+				background-color: red;
+			}
+		}
+
+		&__radio {
+			display: none;
+		}
+
+		&__group {
+			display: flex;
+			align-items: center;
+		}
 	}
 </style>
