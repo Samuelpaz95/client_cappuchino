437567aa42452984d78466475d7f02cbd06265b3
Alan Benavides <bruno@coastt.org>
2021-08-20 10:08:32 -0400

    update: add types in composable useSchedules

--- src/composables/useSchedules.ts
+++ src/composables/useSchedules.ts
@@ -1,10 +1,14 @@
-import { computed } from "vue";
+import { computed, ComputedRef } from "vue";
 import { useStore } from "vuex";
+
+import { scheduleSubject } from "../interfaces";
 
 export function useSchedules() {
 	const store = useStore();
 
-	const schedules = computed(() => store.getters["scheduleSubjects/scheduleSubjects"]);
+	const schedules: ComputedRef<scheduleSubject[]> = computed(
+		() => store.getters["scheduleSubjects/scheduleSubjects"]
+	);
 
 	return { schedules };
 }
