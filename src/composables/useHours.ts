3a37913cc4e4dac57f626eaf9e5d51ccfa7cc578
Alan Benavides <bruno@coastt.org>
2021-09-03 15:33:40 -0400

    add:  compare hours

--- src/composables/useHours.ts
+++ src/composables/useHours.ts
@@ -4,11 +4,16 @@ import { HOURS } from "../enums/hours";
 export function useHours() {
 	const hours = ref(HOURS);
 
-	const semanticHours = () =>
-		hours.value.map((hour) => hour.slice(0, hour.length > 3 ? 2 : 1) + ":" + hour.slice(-2));
+	const semanticHours = () => hours.value.map(convertToSemanticHour);
+
+	const convertToSemanticHour = (hour: string) =>
+		hour.slice(0, hour.length > 3 ? 2 : 1) + ":" + hour.slice(-2);
+
+	const compareHours = (hour: string, semanticHour: string) => semanticHour == convertToSemanticHour(hour);
 
 	return {
 		hours,
 		semanticHours: semanticHours(),
+		compareHours,
 	};
 }
