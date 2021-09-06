56b88e689d59fd99fdc434ffed490a91a3723a74
Alan Benavides <bruno@coastt.org>
2021-09-06 15:22:19 -0400

    add: highlight schedule conflicts

--- src/components/scheduleTable/ScheduleTableItem.vue
+++ src/components/scheduleTable/ScheduleTableItem.vue
@@ -1,5 +1,9 @@
 <template>
-	<div v-for="schedule in schedules" :key="schedule.key" class="schedule-item">
+	<div
+		v-for="schedule in schedules"
+		:key="schedule.key"
+		:class="[{ 'schedule-item__conflictive': conflictiveSchedule }, 'schedule-item']"
+	>
 		<div>{{ schedule.subjectName }}</div>
 		<span>G {{ schedule.groupCode }}</span> - <span>A {{ schedule.room }}</span>
 	</div>
@@ -18,16 +22,25 @@
 				type: Array as PropType<scheduleTable[]>,
 			},
 		},
-		setup() {
-			return {};
+		computed: {
+			conflictiveSchedule(): Boolean {
+				return this.schedules.length > 1;
+			},
 		},
 	});
 </script>
 
 <style lang="scss" scoped>
+	@import "@/scss/abstracts/variables.scss";
+
 	.schedule-item {
 		margin: 10px;
 		font-size: 1em;
 		line-height: 16px;
+
+		&__conflictive {
+			color: $secondary_color;
+			font-weight: 700;
+		}
 	}
 </style>
