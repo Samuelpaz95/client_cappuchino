dfded80455c4d9957c40041344b298874c449ec3
Alan Benavides <bruno@coastt.org>
2021-11-12 17:03:13 -0400

    feature: action search

---
+++ src/composables/useTeacherTracker.ts
@@ -0,0 +1,32 @@
+import { inject, onUnmounted } from "vue";
+import { useStore } from "vuex";
+import professorsService from "../services/professors";
+
+export function useTeacherTrakcer() {
+	const department = inject("currentDepartment") as string;
+	const store = useStore();
+
+	const searchDocent = async (professor: string) => {
+		store.commit("scheduleSubjects/removeAllScheduleSubjects");
+
+		const subjects = await professorsService.getOneByDepartment(department, professor);
+		if (subjects) {
+			subjects.forEach((subject, index) => {
+				subject.groups.forEach((group) => {
+					store.commit("scheduleSubjects/addScheduleSubjects", {
+						key: index,
+						subjectName: subject.name,
+						groupCode: group.code,
+						schedule: group.schedule,
+					});
+				});
+			});
+		}
+	};
+
+	onUnmounted(() => {
+		store.commit("scheduleSubjects/removeAllScheduleSubjects");
+	});
+
+	return { searchDocent };
+}
