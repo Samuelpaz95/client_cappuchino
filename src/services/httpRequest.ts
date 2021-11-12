f16ac90be53a979f80650c0f2927eea3b11b62f4
Alan Benavides <bruno@coastt.org>
2021-11-12 18:50:18 -0400

    fix: comment importMetaEnvInterface

--- src/services/httpRequest.ts
+++ src/services/httpRequest.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const http: AxiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_APP_API_SERVER,
+	baseURL: import.meta.env.VITE_APP_API_SERVER as string,
 	headers: { "Content-Type": "application/json" },
 });
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
-	readonly VITE_APP_API_SERVER: string;
-}
+// interface ImportMetaEnv extends Readonly<Record<string, string>> {
+// 	readonly VITE_APP_API_SERVER: string;
+// }
 
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
+// interface ImportMeta {
+// 	readonly env: ImportMetaEnv;
+// }
