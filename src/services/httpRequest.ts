0acd818534e8b0a0fc863503d601f8a44f3d86e0
Alan Benavides <bruno@coastt.org>
2021-11-12 18:19:33 -0400

    add: enviroment key server api

--- src/services/httpRequest.ts
+++ src/services/httpRequest.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const http: AxiosInstance = axios.create({
-	baseURL: "http://localhost:5000",
+	baseURL: import.meta.env.VITE_APP_API_SERVER,
 	headers: { "Content-Type": "application/json" },
 });
 
