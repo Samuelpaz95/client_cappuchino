5dee9dc87f636335987dc5f76867db419b49d0f2
Alan Benavides <bruno@coastt.org>
2021-11-12 20:12:14 -0400

    add: app ruotes in main

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,87 @@
 <template>
-	<h1>Home</h1>
+	<main class="home">
+		<h1>SCESI</h1>
+		<nav class="home__nav">
+			<router-link
+				v-for="{ name, routeName, department } of routes"
+				:to="{ name: routeName, params: { department: department } }"
+				class="home__link"
+			>
+				{{ name }}
+			</router-link>
+		</nav>
+	</main>
 </template>
 
-<script lang="ts">
-	import { defineComponent } from "vue";
-
-	export default defineComponent({
-		name: "Home",
-	});
+<script setup lang="ts">
+	const routes = [
+		{
+			name: "Cappuchino FCYT",
+			routeName: "Cappuchino",
+			department: "fcyt",
+		},
+		{
+			name: "Cappuchino FCE",
+			routeName: "Cappuchino",
+			department: "fce",
+		},
+		{
+			name: "Teacher Tracker FCYT",
+			routeName: "TeacherTracker",
+			department: "fcyt",
+		},
+		{
+			name: "Teacher Tracker FCE",
+			routeName: "TeacherTracker",
+			department: "fce",
+		},
+	];
 </script>
 
-<style></style>
+<style scoped lang="scss">
+	@import "@/scss/abstracts/variables", "@/scss/abstracts/mixins";
+	.home {
+		display: grid;
+		grid-template-rows: auto 1fr;
+		padding: 1rem 2rem;
+		height: 100%;
+
+		@include large {
+			padding: 1rem;
+		}
+
+		&__nav {
+			display: grid;
+			gap: 3rem;
+			padding: 3rem 0;
+			grid-template-rows: repeat(1fr, 4);
+
+			@include large {
+				padding: 0;
+				gap: 1rem;
+				grid-template-columns: 1fr 1fr;
+				grid-template-rows: 1fr 1fr;
+			}
+		}
+
+		&__link {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			text-align: center;
+			border-radius: $border_radius;
+			background-color: $primary_color;
+			padding: 1rem;
+			color: $font_color;
+			text-decoration: none;
+			font-weight: bold;
+			font-size: 1.4rem;
+
+			@include large {
+				height: 50%;
+				width: 70%;
+				place-self: center;
+			}
+		}
+	}
+</style>
