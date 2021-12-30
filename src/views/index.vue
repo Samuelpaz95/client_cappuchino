2ca1a853a1dfca4eca8272b6c22da4040c9bd7a1
Alan Benavides <bruno@coastt.org>
2021-12-30 00:44:07 -0400

    ui: home screen

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,65 +1,88 @@
 <template>
 	<main class="home">
 		<nav class="home__nav">
-			<router-link
-				v-for="{ name, routeName, department } of routes"
-				:to="{ name: routeName, params: { department: department } }"
-				class="home__link"
-			>
-				{{ name }}
-			</router-link>
+			<div class="home__department" v-for="[title, links] of Object.entries(options)">
+				<h2>{{ title }}</h2>
+				<div class="home_links">
+					<router-link
+						v-for="{ name, routeName, department } of links"
+						:to="{ name: routeName, params: { department: department } }"
+						class="home__link"
+					>
+						{{ name }}
+					</router-link>
+				</div>
+			</div>
 		</nav>
 	</main>
 </template>
 
 <script setup lang="ts">
-	const routes = [
-		{
-			name: "Cappuchino FCYT",
-			routeName: "Cappuchino",
-			department: "fcyt",
-		},
-		{
-			name: "Cappuchino FCE",
-			routeName: "Cappuchino",
-			department: "fce",
-		},
-		{
-			name: "Teacher Tracker FCYT",
-			routeName: "TeacherTracker",
-			department: "fcyt",
-		},
-		{
-			name: "Teacher Tracker FCE",
-			routeName: "TeacherTracker",
-			department: "fce",
-		},
-	];
+	const options = {
+		"Facultad de Ciencias y Tecnologia": [
+			{
+				name: "Cappuchino",
+				routeName: "Cappuchino",
+				department: "fcyt",
+			},
+			{
+				name: "Teacher Tracker",
+				routeName: "TeacherTracker",
+				department: "fcyt",
+			},
+		],
+		"Facultad de Ciencias Economicas": [
+			{
+				name: "Cappuchino",
+				routeName: "Cappuchino",
+				department: "fce",
+			},
+			{
+				name: "Teacher Tracker",
+				routeName: "TeacherTracker",
+				department: "fce",
+			},
+		],
+	};
 </script>
 
 <style scoped lang="scss">
 	@import "@/scss/abstracts/variables", "@/scss/abstracts/mixins";
 	.home {
+		height: 100%;
 		display: grid;
 		grid-template-rows: 1fr;
 		padding: 1rem 2rem;
-		height: 100%;
-
-		@include large {
-			padding: 1rem;
-		}
 
 		&__nav {
+			height: 100%;
 			display: grid;
-			gap: 3rem;
-			padding: 3rem 0;
-			grid-template-rows: repeat(1fr, 4);
+			grid-template-rows: 1fr 1fr;
+		}
+
+		&__department {
+			height: 100%;
+			display: grid;
+			gap: 2rem;
+			grid-template-rows: 50px 1fr;
 
 			@include large {
-				padding: 0;
-				gap: 1rem;
+				gap: 0;
+			}
+		}
+
+		&_links {
+			height: 100%;
+			display: grid;
+			grid-template-rows: 1fr 1fr;
+			grid-template-columns: 1fr;
+			gap: 1rem;
+
+			@include large {
+				height: 100%;
+				display: grid;
+				grid-template-rows: 1fr;
 				grid-template-columns: 1fr 1fr;
-				grid-template-rows: 1fr 1fr;
 			}
 		}
 
