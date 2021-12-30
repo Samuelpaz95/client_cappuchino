7c0f8cef5bb13f2956490f69864e120a328d01b8
Alan Benavides <bruno@coastt.org>
2021-12-29 21:36:16 -0400

    add: colors for schedules

---
+++ src/constants/colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+	DARK: [
+		"#aeda00",
+		"#efd828",
+		"#ff6363",
+		"#2490fc",
+		"#62c8ab",
+		"#bba1ff",
+		"#e8c921",
+		"#f3c243",
+		"#c1f7d8",
+		"#e66cef",
+		"#f97308",
+	],
+	LIGHT: ["#d89b96", "#c4d7d1", "#bddee5", "#b29299", "#e2d3a8", "#e7ad87"],
+};
