3258d239e1946ea840b8eddbfe70f041a18c2e1a
Alan Benavides <bruno@coastt.org>
2022-02-06 15:39:55 -0400

    add: layout not found page

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,11 +1,58 @@
-<template><p>Pagina no encontrada :p</p></template>
+<template>
+	<div class="not-found">
+		<div>
+			<h1>Este Recurso no esta disponible</h1>
+			<div class="not-found__buttons">
+				<button class="not-found__button" @click="$router.push('/')">
+					<BaseSystemIcons icon="home" height="30" /><span>Inicio</span>
+				</button>
+				<button class="not-found__button" @click="$router.go(-1)">
+					<BaseSystemIcons icon="angle" :rotate="270" height="30" /><span>Atras</span>
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
 
-<script lang="ts">
-	import { defineComponent } from "vue";
-
-	export default defineComponent({
-		name: "404",
-	});
+<script setup lang="ts">
+	import BaseSystemIcons from "../../components/ui/BaseSystemIcons.vue";
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	@import "@/scss/abstracts/mixins";
+
+	.not-found {
+		display: grid;
+		place-items: center;
+		min-height: 100vh;
+
+		h1 {
+			padding: 2rem;
+			text-align: center;
+			margin: 0;
+		}
+
+		&__buttons {
+			@include large {
+				display: flex;
+				flex-direction: row-reverse;
+				justify-content: space-between;
+			}
+		}
+
+		&__button {
+			display: flex;
+			align-items: center;
+			justify-content: space-evenly;
+
+			background-color: transparent;
+			color: inherit;
+			border: 1px solid var(--secondary-color);
+			padding: 1rem;
+			margin: 2rem;
+			font-size: 2rem;
+			font-weight: 700;
+			width: calc(100% - 4rem);
+		}
+	}
+</style>
